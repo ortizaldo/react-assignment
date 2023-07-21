6eaddfe4dddd1206a1cb4c1547a302d7c2e1e69a
Aldo Ortiz <ortizaldo14@gmail.com>
2023-07-21 14:42:18 -0600

    se quita codigo comentado

--- src/components/Cardcontainer/Card.js
+++ src/components/Cardcontainer/Card.js
@@ -78,39 +78,6 @@ export default function Movie(props) {
               <Row className={clsx("text-2xl font-bold", titleFont.className)}>
                 <h2 className={clsx(titleFont.className)}>{original_title}</h2>
               </Row>
-
-              {/* <Row>
-                <Col>
-                  <div
-                    className={clsx(
-                      "grid grid-flow-col auto-cols-max text-color",
-                      titleFont.className
-                    )}
-                  >
-                    {resultados.map((objeto, idx) => {
-                      idx += 1;
-                      return (
-                        <div
-                          className="col-4 ml-2"
-                          style={{ color: "#0073b1" }}
-                        >
-                          <Tag value={objeto.name} />
-                        </div>
-                      );
-                    })}
-                  </div>
-                </Col>
-              </Row> */}
-              {/* <Row>
-                <Col>
-                  <a
-                    className="btn"
-                    href={`/${type}/${genSlug(title || name, id)}`}
-                  >
-                    {title || name}
-                  </a>
-                </Col>
-              </Row> */}
             </Container>
           </div>
           <div className="row">
@@ -119,90 +86,6 @@ export default function Movie(props) {
             </div>
           </div>
         </div>
-        {/* <div className="corner" style={{ width: 60, height: 60 }}>
-          <Knob
-            value={average}
-            onChange={(e) => setValue(e.value)}
-            min={0}
-            max={100}
-            strokeWidth={10}
-            size={60}
-          />
-        </div>
-        <Container>
-          <Row>
-            <Image
-              src={`https://image.tmdb.org/t/p/w500${poster_path}`}
-              alt={original_title}
-              width="300"
-              height="350"
-            />
-          </Row>
-          <Row>
-            <div className="descriptions">
-              <Container className="grid grid-flow-row auto-rows-max">
-                <Row
-                  className={clsx("text-2xl font-bold", titleFont.className)}
-                >
-                  <h5
-                    className={clsx("text-xl font-bold", titleFont.className)}
-                  >
-                    {original_title}
-                  </h5>
-                </Row>
-
-                <Row>
-                  <Col></Col>
-                  <Col>
-                    <div
-                      className={clsx(
-                        "grid grid-flow-col auto-cols-max text-color",
-                        titleFont.className
-                      )}
-                    >
-                      <div className="ml-2" style={{ color: "#0073b1" }}>
-                        <Tag value={release_date}></Tag>
-                      </div>
-                    </div>
-                  </Col>
-                </Row>
-
-                <Row>
-                  <Col>
-                    <div
-                      className={clsx(
-                        "grid grid-flow-col auto-cols-max text-color",
-                        titleFont.className
-                      )}
-                    >
-                      {resultados.map((objeto, idx) => {
-                        idx += 1;
-                        return (
-                          <div
-                            className="col-4 ml-2"
-                            style={{ color: "#0073b1" }}
-                          >
-                            <Tag value={objeto.name} />
-                          </div>
-                        );
-                      })}
-                    </div>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col>
-                    <a
-                      className="btn"
-                      href={`/${type}/${genSlug(title || name, id)}`}
-                    >
-                      {title || name}
-                    </a>
-                  </Col>
-                </Row>
-              </Container>
-            </div>
-          </Row>
-        </Container> */}
       </div>
     </>
   );
